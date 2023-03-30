247d5c582ba90afec0d7faadca108b622bea2bef
Vivek Murali <vivekmurali00@outlook.com>
2023-03-30 09:14:13 +0530

    Added Airflow Image with Kafka Dependency

---
+++ models/snowflake.py
@@ -0,0 +1,14 @@
+from typing import *
+
+class Snowflake:
+    def __init__(self):
+        API_KEY = ""
+    
+    def __str__(self):
+        pass
+    
+    def __getattribute__(self, __name: str) -> Any:
+        pass
+    
+    
+    
